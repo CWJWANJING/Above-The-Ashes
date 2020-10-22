635bca9a4c5de205610a845dda660bf620fd7b69
CWJWANJING <wanjingchencwj@gmail.com>
2020-10-22 18:51:58 +0100

    Added a sliding-open chest
    Still one issue: the trigger collider cannot let player pass through it even isTrigger box is clicked.

---
+++ Above The Ashes/Assets/scripts/chestTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chestTrigger : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+      anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+      anim.SetTrigger("openlid");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+      anim.enabled = true;
+    }
+
+    void pauseAnimationEvent()
+    {
+      anim.enabled = false;
+    }
+
+}
