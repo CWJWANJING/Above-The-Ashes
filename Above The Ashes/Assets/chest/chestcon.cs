b52b48347745407eec0de25dbf1e92a0eb4077e4
CWJWANJING <wanjingchencwj@gmail.com>
2020-10-30 11:21:40 +0000

    Added some new interactions with the environment
    -Added a new chest box, which has particles in it and can click open, however, cannot close at this state
    -Added a hint text when the player is near the chest, "Click to open the chest" will show on the screen.

---
+++ Above The Ashes/Assets/chest/chestcon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chestcon : MonoBehaviour
+{
+    // get the "Click to open the chest" text
+    public GameObject UIObject;
+
+    void Start()
+    {
+      // at first the text should not display
+      UIObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // when player is near the chest
+        if (other.tag == "Player")
+        {
+            // show the text
+            UIObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // when player leaves the trigger, text disappear
+        UIObject.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+      // when player clicks on the chest, the chest lid will open
+      GetComponent<Rigidbody>().angularVelocity= new Vector3(.8f,0,0);
+      StartCoroutine(stopOpening());
+    }
+
+    // this allows chest from continuing opening
+    IEnumerator stopOpening()
+    {
+      yield return new WaitForSeconds (1.6f);
+      GetComponent<Rigidbody>().angularVelocity= new Vector3(0,0,0);
+    }
+}
