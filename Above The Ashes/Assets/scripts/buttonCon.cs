954b4073ecfc37bbd418bab4c8a03fc335624bf3
CWJWANJING <wanjingchencwj@gmail.com>
2020-11-10 15:36:25 +0000

    Image Trigger
    Added a trigger cube, which will display an welcome image

---
+++ Above The Ashes/Assets/scripts/buttonCon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonCon : MonoBehaviour
+{
+
+    public Image hintImage;
+
+    void Start()
+    {
+      hintImage.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // when player is near the chest
+        if (other.tag == "Player")
+        {
+            // show the text
+            hintImage.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // when player leaves the trigger, text disappear
+        hintImage.enabled = false;
+    }
+}
