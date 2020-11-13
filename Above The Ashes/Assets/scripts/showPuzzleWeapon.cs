f7fd4edcbc21b9c395dcb28a097bbdd5bb4f21d0
CWJWANJING <wanjingchencwj@gmail.com>
2020-11-13 11:47:20 +0000

    Switched from mouse click to key press
    The trigger for the puzzle is now triggered by the f key, and can be escaped by escape key.
    
    When the puzzle complete, the image will disappear now.
    
    Players can only enter the puzzle when they are close to the trigger object

--- Above The Ashes/Assets/scripts/showPuzzleWeapon.cs
+++ Above The Ashes/Assets/scripts/showPuzzleWeapon.cs
@@ -6,16 +6,37 @@ using UnityEngine.UI;
 public class showPuzzleWeapon : MonoBehaviour
 {
   public Image puzzleImage;
+  public GameObject player;
 
   void Start()
   {
     puzzleImage.gameObject.SetActive(false);
   }
 
-  void OnMouseDown()
-  {
-    puzzleImage.gameObject.SetActive(true);
-  }
-
+  void Update()
+    {
+      // if player is closenough with this object
+      if (Vector3.Distance(this.gameObject.transform.position, player.transform.position) < 3)
+      {
+        // if the player press key F
+        if (Input.GetKey("f"))
+        {
+            // the puzzle will appear
+            puzzleImage.gameObject.SetActive(true);
+        }
+        // if player success in the puzzle
+        if (puzzleControl.win)
+        {
+          // the puzzle image will disappear
+          puzzleImage.gameObject.SetActive(false);
+        }
+        // if escape key is pressed
+        if (Input.GetKey(KeyCode.Escape))
+        {
+          // image will disappear
+          puzzleImage.gameObject.SetActive(false);
+        }
+      }
+    }
 
 }
