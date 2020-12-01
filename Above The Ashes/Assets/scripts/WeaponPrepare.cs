d85dca5901130e2e74085d96a725d5d5224475ef
Liang_Xiaowei <38693759+QRr0w@users.noreply.github.com>
2020-12-01 17:36:40 +0800

    Add weapon
    Simple weapon

---
+++ Above The Ashes/Assets/scripts/WeaponPrepare.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPrepare : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject gs;
+    private GameObject camera;
+    private GameObject player;
+    public GameObject weapon_ready;
+    public GameObject weapon_notR;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gs = GameObject.FindGameObjectWithTag("GS");
+        player = GameObject.FindGameObjectWithTag("Player");
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        if (camera.GetComponent<TPSCamera>().isAiming)
+        {
+            weapon_ready.SetActive(true);
+            weapon_notR.SetActive(false);
+        }
+        else {
+            weapon_ready.SetActive(false);
+            weapon_notR.SetActive(true);
+        }
+
+    }
+}
