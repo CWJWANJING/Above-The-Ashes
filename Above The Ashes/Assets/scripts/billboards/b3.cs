33a67333fc16d0b4e6272c1774cd9cdd22728d61
WANJINGCWJ <wanjingchencwj@gmail.com>
2020-12-02 17:00:05 +0000

    finished all the billboards

---
+++ Above The Ashes/Assets/scripts/billboards/b3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class b3 : MonoBehaviour
+{
+  public GameObject Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text.SetActive(false);
+    }
+
+    void OnTriggerEnter()
+    {
+      Text.SetActive(true);
+    }
+
+    void OnTriggerExit()
+    {
+      Text.SetActive(false);
+    }
+}
