9b9dc4d44839980fb7777a06451e7b80c7f036e7
Liang_Xiaowei <38693759+QRr0w@users.noreply.github.com>
2020-12-03 20:41:46 +0800

    Update story
    Update story
    Fix bugs about billboard

---
+++ Above The Ashes/Assets/Images/Level2/L2exitControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2exitControl : MonoBehaviour
+{
+
+  Animator anim;
+  // public TextMesh howmanykeys;
+  // public static int N = 0;
+  void Start()
+  {
+    anim = GetComponent<Animator>();
+    // howmanykeys.gameObject.enabled = false;
+  }
+
+  void Update()
+  {
+    // show how many keys have been collected
+    // Debug.Log(N);
+    // if (N>0)
+    // {
+    //   howmanykeys.gameObject.enabled = true;
+    //   howmanykeys.text = "Numer of keys found: "+N+ToString();
+    // }
+
+    if (L2key1.key1Found == true && L2key2.key2Found == true && L2key3.key3Found == true)
+    {
+      anim.SetTrigger("openL2door");
+    }
+  }
+
+  void pauseAnimationEvent()
+  {
+    anim.enabled = false;
+  }
+}
---
+++ Above The Ashes/Assets/Images/Level2/Level2Notify.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Notify : MonoBehaviour
+{
+  public GameObject player;
+  public GameObject UIObject;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    // at first the text should not display
+    UIObject.SetActive(false);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // if player is closenough with this object
+    if (Vector3.Distance(this.gameObject.transform.position, player.transform.position) < 3)
+    {
+      // tell user to press f to pick up
+      UIObject.SetActive(true);
+    }
+    else{
+      UIObject.SetActive(false);
+    }
+  }
+}
